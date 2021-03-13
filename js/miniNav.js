4c8efb4e68483da460dadb7a31f2b35f4708c6d2
Ken <kwall925@gmail.com>
2021-03-13 20:15:27 -0300

    home nav problems solved

--- js/miniNav.js
+++ js/miniNav.js
@@ -5,19 +5,19 @@ const miniNav = () => {
 
 const miniNavAppear = () => {
   let showMe = document.querySelector(".miniNav");
-  showMe.style.cssText = 'display: flex; flex-direction: column; justify-content: center; align-items: flex-start; font-size: 5em;  list-style-type: none; position: fixed; top: 0; left: 0; background: white; height: 100%; width: 60vw; z-index: 10; ';
+  showMe.style.cssText = 'display: flex; flex-direction: column; justify-content: center; align-items: flex-start; font-size: 5em;  list-style-type: none; position: fixed; top: 0; left: 0; background: rgba(255, 255, 255, 0.9); height: 100%; width: 67vw; z-index: 10; ';
   showMe.style.transition = "2s";
   }
 
 const closeMini = () => {
-  let closeBtn = document.getElementById("closeNav");
-  closeBtn.onclick = navDisappear;
+  let closeBtn = document.querySelector(".close");
+  closeBtn.onclick = miniDisappear;
 }
 
 const miniDisappear = () => {
-  let showMe = document.querySelector(".classicNav");
-  showMe.style.opacity = 0;
-  showMe.style.transition = ".3s";
+  let showMe = document.querySelector(".miniNav");
+  showMe.style.display = 'none';
+  showMe.style.transition = ".8s";
 }
 
 window.addEventListener('load', function () {
