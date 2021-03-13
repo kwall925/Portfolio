7fb4af83c68639f3b8425c19aa34af3c5036e2dd
Ken <kwall925@gmail.com>
2021-03-13 19:12:32 -0300

    continued testing of responsive navs

--- js/miniNav.js
+++ js/miniNav.js
@@ -1,17 +1,17 @@
 const miniNav = () => {
   let navbtn = document.querySelector(".burger");
-  navbtn.onclick = navAppear;
+  navbtn.onclick = miniNavAppear;
 }
 
 const miniNavAppear = () => {
-let showMe = document.querySelector(".classicNav");
-showMe.style.display = flex;
-showMe.style.transition = "2s";
-}
+  let showMe = document.querySelector(".miniNav");
+  showMe.style.cssText = 'display: flex; flex-direction: column; position: fixed; top: 0; left: 0; background: white; height: 100%; width: 60vw; z-index: 10; ';
+  showMe.style.transition = "2s";
+  }
 
 const closeMini = () => {
-let closeBtn = document.getElementById("closeNav");
-closeBtn.onclick = navDisappear;
+  let closeBtn = document.getElementById("closeNav");
+  closeBtn.onclick = navDisappear;
 }
 
 const miniDisappear = () => {
